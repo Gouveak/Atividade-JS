565e2b03fd2f392342bc627f1036a46ceaf99874
gouveak <kaua.gouvea01@etec.sp.gov.br>
2026-02-22 12:50:26 -0300

    Atualização

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ function AdicionarProduto() {
     let preco = document.getElementById("preco").value
     let quantidade = document.getElementById("quantidade").value
     lista.push({
+        indice: lista.length,
         nome: produto,
         preco: Number(preco),
-        quantidade: Number(quantidade)
+        quantidade: Number(quantidade),
+        subtotal: Number(preco) * Number(quantidade)
     })
     document.getElementById("produto").value = ""
     document.getElementById("preco").value = ""
@@ -21,9 +23,11 @@ function ListarProdutos() {
     for (let i = 0; i < lista.length ; i ++) {
         tbody.innerHTML += `
         <tr>
+            <td>${lista[i].indice}</td>
             <td>${lista[i].nome}</td>
             <td>${lista[i].preco}</td>
             <td>${lista[i].quantidade}</td>
+            <td>${lista[i].subtotal}</td>
         </tr>
         
         `
@@ -35,15 +39,36 @@ function Limpar() {
     lista = []
     document.getElementById("resultado").innerHTML = ""
     document.getElementById("totalproduto").innerHTML = 'Todos os produtos foram removidos.'
+    document.getElementById("resumo").innerHTML = ""
+    document.getElementById("remover").innerHTML = ""
 }
 
 function Resumir() {
+    let subTotal = 0
     let totalQuantidade = 0
-    for (let i = 0; i < lista.lenght; i++) {
-        totalQuantidade += lista[i].quantidade  
+    for (let i = 0; i < lista.length; i++) {
+        totalQuantidade = totalQuantidade + lista[i].quantidade  
+    }
+    for (let i = 0; i < lista.length; i++) {
+        subTotal = subTotal + lista[i].subtotal
     }
     document.getElementById("resumo").innerHTML = `
-    <p> Total de produtos: ${lista.length}</p>
-    <p> Quantidade total: ${totalQuantidade}</p>
+    <h2> Resumo </h2>
+    <p> Total de produtos: <strong>${lista.length}</strong></p>
+    <p> Total de itens: <strong>${totalQuantidade}</strong></p>
+    <p> Valor total em estoque: <strong>R$ ${subTotal}</strong></p>
     `
+}
+function RemoverIndice() {
+    
+    let indice = Number(document.getElementById("indice").value)
+    if (indice >= 0 && indice < lista.length ) {
+        lista.splice(indice, 1)
+        document.getElementById("remover").innerHTML = `Produto removido.`
+    }
+    document.getElementById("indice").value = ""
+
+
+
+
 }
